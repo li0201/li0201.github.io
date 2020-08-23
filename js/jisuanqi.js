6eb9a97c48d6b16001e7905a415e364c19d39d0d
li0201 <li0201@35168463@qq.com>
2020-08-23 22:44:02 +0800

    解决了计算器的精度误差

--- js/jisuanqi.js
+++ js/jisuanqi.js
@@ -6,14 +6,44 @@ function isnumber(obj) {
 	}
 	return false;
 }
+function isinteger(numb) {
+	return Math.floor(numb) === numb;
+}
+function tointeger(num1,num2) {
+	var ret={num_1:0,num_2:0,times:0};
+	if (isinteger(num1)&&isinteger(num2)){
+		ret.num_1=num1;
+		ret.num_2=num2;
+		return ret;
+	}
+	while (!isinteger(num1)||!isinteger(num2)){
+		++ret.times;
+		num1*=10;
+		num2*=10;
+	}
+	ret.num_1=num1;
+	ret.num_2=num2;
+	ret.times=Math.pow(10,ret.times);
+	return ret;
+}
+function ys(num1,num2) {
+	var k=tointeger(num1,num2);
+	var add=(k.num_1+k.num_2)/k.times;
+	var subtact=(k.num_1-k.num_2)/k.times;
+	var multiply=(k.num_1*k.num_2)/(k.times*k.times);
+	var divide=(k.num_1/k.num_2);
+	var ans={add,subtact,multiply,divide};
+	return ans;
+}
 function init() {
 	var number1=document.getElementById("input1").value;
 	var number2=document.getElementById("input2").value;
 	if (isnumber(number1)&&isnumber(number2)){
-		document.getElementById("demo1").innerHTML=Number(number1)+Number(number2);
-		document.getElementById("demo2").innerHTML=Number(number1)-Number(number2);
-		document.getElementById("demo3").innerHTML=Number(number1)*Number(number2);
-		document.getElementById("demo4").innerHTML=Number(number1)/Number(number2);
+		var result=ys(number1,number2);
+		document.getElementById("demo1").innerHTML=result.add;
+		document.getElementById("demo2").innerHTML=result.subtact;
+		document.getElementById("demo3").innerHTML=result.multiply;
+		document.getElementById("demo4").innerHTML=result.divide;
 	}
 	else {
 		window.alert("请输入正确格式的数字");
